9dec3b9be8032d2066f47592785de9b72841b4db
Mehul Gupta <mehulgupta2409@Mehuls-MacBook-Air.local>
2024-09-03 14:46:30 +0530

    s

--- JavaScript/script3.js
+++ JavaScript/script3.js
@@ -32,13 +32,13 @@
 // console.log(Shop.budget)
 
 function mehul(){
-    console.log(this)
+    // console.log(this)
 }
 // mehul()
 
 const print = function(){
     for(let i=0;i<5;i++){
-        console.log(i);
+        // console.log(i);
     }
 }
 // print()
@@ -50,13 +50,13 @@ const print = function(){
 // } 
 // printt()
 
-const some_print  = (num1,num2) =>{
-    while(num1!=0 || num2!=0){
-        console.log(num1+num2);
-        num1--;
-        num2--;
-    }
-}
+// const some_print  = (num1,num2) =>{
+//     while(num1!=0 || num2!=0){
+//         console.log(num1+num2);
+//         num1--;
+//         num2--;
+//     }
+// }
 
 // const add_two_num = (num1,num2) => (num1*num2)
 
@@ -72,3 +72,69 @@ const some_print  = (num1,num2) =>{
 // }
 // a=a+3
 // console.log(a)
+// alert("g")  // we are using node.js not browser
+
+// null - stanalone value
+// undefined - when value is not assigned
+
+// symbol -> unique
+
+// object - 
+
+// console.log(Number("3")==3)
+
+// Datatypes - 1)Primitive 2) Non - Primitive
+// primitive - String,Number,Boolean,null,undefined,Symbol,BigInt(after 10^53)    all this is pass by value call in function
+// non primitive - Arrays,Objects,Functions    (pass by reference value)
+// JavaScript - not defining the datatype ,ex const email=92 or const email="mehulgupta@gmail.com" - means can be string or number , we are not defining it earlier
+// means JavaScript is dynamically type language
+
+// const bignumber=894849233328443n //then inside BigInt is only used
+// console.log(typeof(bignumber))
+
+// object are those which are written in curly braces for example
+const myObject={
+    name:"Mehul Gupta",
+    age:22
+}
+
+let myFun = function(){
+// console.log("l");
+}
+// d3
+// myFun()
+// console.log(typeof(myFun))
+
+// Memory - two types of Memory - Stack , Heap
+// Stack memory - primitive data type  (Copy banta h)
+// Heap memory - Non primitive data type   (reference of original value)
+
+// const name =prompt("enter number ");
+// console.log(`Hello my name is ${name}`)
+// Prompt the user for input
+// const userInput = prompt("Please enter your name:");
+
+// // Display the input
+// console.log("User's name is: " + userInput);
+// // Prompt the user to enter a number
+// const userInput = prompt("Please enter a number:");
+
+// // // Convert the input to a number
+// const number = Number(userInput);
+
+// // Display the number
+// alert("The number you entered is: " + number);
+// console.log("The number you entered is:", number);
+
+const name=new String("Mhulee");
+// console.log(name.length)
+// console.log(name.indexOf('e'))
+
+let myDate=new Date();
+console.log(myDate)
+console.log(myDate.toString())
+console.log(myDate.toDateString())
+console.log(myDate.toLocaleString())
+
+// shallow copy - pass by reference - on heap 
+// deep copy - pass by value(do not share same reference points) - on stack 
