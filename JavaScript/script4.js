d3793c67dc64a79b420784416781be42322a6ae3
Mehul Gupta <mehulgupta2409@Mehuls-MacBook-Air.local>
2024-09-06 22:41:37 +0530

    2

--- JavaScript/script4.js
+++ JavaScript/script4.js
@@ -16,20 +16,32 @@
 // document.querySelector('.class') //hash for selecting by class
 
 // document.querySelectorAll('.bg-black')
-const myheading=document.querySelector('h1')
-const paraInsideMyHeading=myheading.querySelector('p')
-paraInsideMyHeading.style.backgroundColor='green'
+// const myheading=document.querySelector('h1')
+// const paraInsideMyHeading=myheading.querySelector('p')
+// paraInsideMyHeading.style.backgroundColor='green'
 
 
-const LiList=document.querySelectorAll('ul')
+// const LiList=document.querySelectorAll('ul')
 // LiList.style.backgroundColor('green') - will give error
 // LiList[0].style.backgroundColor('red) - won't give error - but also don't consider it as full array
 // LiList[1].style.backgroundColor('red) - won't give error - but also don't consider it as full array
 
-LiList.forEach(function chai(listt){
-    listt.querySelector.style.backgroundColor='red'
-})
+// LiList.forEach(function chai(listt){
+//     listt.querySelector.style.backgroundColor='red'
+// })
 
 // if we want to convert any list to array we use 
 // let li is list
-Array.from(li)
+// Array.from(li)
+// then we can use map function
+// storing it below
+// const storeArray=Array.from(i)
+
+const key=document.querySelectorAll('h2')
+const to_arr=Array.from(key)
+to_arr.forEach((each_h2)=>{
+    each_h2.style.backgroundColor='red';
+    each_h2.style.fontSize='20px';
+    each_h2.style.color='red';
+})
+
