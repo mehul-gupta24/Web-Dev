7d04e19849e152303d140b19a968bb2916a9f561
Mehul Gupta <mehulgupta2409@Mehuls-MacBook-Air.local>
2024-09-06 19:52:40 +0530

    2

--- JavaScript/script4.js
+++ JavaScript/script4.js
@@ -1,1 +1,35 @@
 // DOM - document object model
+
+// innerText - jo dikh rha h website pe wahi milega
+// textContent - jo code me hai woh dikhega
+// innerHTML - gives all html code inside of it(written in code)
+
+
+// Query Selector
+
+// document.querySelector('h1') 
+//     - will give 1st h1 para inside full content 
+// document.querySelectorAll('p')
+//     - will give all p tag inside content
+
+// document.querySelector('#id') //hash for selecting by id
+// document.querySelector('.class') //hash for selecting by class
+
+// document.querySelectorAll('.bg-black')
+const myheading=document.querySelector('h1')
+const paraInsideMyHeading=myheading.querySelector('p')
+paraInsideMyHeading.style.backgroundColor='green'
+
+
+const LiList=document.querySelectorAll('ul')
+// LiList.style.backgroundColor('green') - will give error
+// LiList[0].style.backgroundColor('red) - won't give error - but also don't consider it as full array
+// LiList[1].style.backgroundColor('red) - won't give error - but also don't consider it as full array
+
+LiList.forEach(function chai(listt){
+    listt.querySelector.style.backgroundColor='red'
+})
+
+// if we want to convert any list to array we use 
+// let li is list
+Array.from(li)
